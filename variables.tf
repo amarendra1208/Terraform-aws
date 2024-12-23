aa24273cab0b136e93b1637b0adbff90a9ae5e6d
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2024-12-23 01:44:12 -0600

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "AKIAUBKFCRQ2DD3FJFW2"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = "tRrauD0EHWJdkqDnn+JWKCBDg4CanBPGzBQa3wrR"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0b4624933067d393a"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
