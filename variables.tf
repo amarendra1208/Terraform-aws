379a8b6b090ea1fedfb4c0a229d717f89f649465
amarendra1208 <108320428+amarendra1208@users.noreply.github.com>
2024-12-23 01:50:24 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
-  default     = "AKIAUBKFCRQ2DD3FJFW2"
+  default     = "AAKKIAUBKFCRQ2DD3FJFW2"
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
   type        = string
-  default     = "tRrauD0EHWJdkqDnn+JWKCBDg4CanBPGzBQa3wrR"
+  default     = "tRARrauD0EHWJdkqDnn+JWKCBDg4CanBPGzBQa3wrR"
 }
 
 variable "aws_region" {
